489a27229c096983489710a7e6d8139df83267e4
jy0813 <busbug3@gmail.com>
2023-10-03 22:01:58 +0900

    feat: data api call

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,3 +1,20 @@
+import { getUser } from '@/apis/login';
+import { useQuery } from '@tanstack/react-query';
+
 export default function List() {
-  return <div>리스트</div>;
+  const { data: userListData, isLoading } = useQuery(['userListData'], getUser);
+  console.log(userListData);
+
+  if (isLoading) {
+    return <div>로딩중입니다.</div>;
+  }
+  return (
+    <div>
+      <ul>
+        {userListData.map((user) => (
+          <li key={user.id}> {user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [loginValue, setLoginValue] = useState({
+    id: '',
+    password: '',
+  });
+
+  const { id, password } = loginValue;
+
+  const handleLoginInput = (e) => {
+    const { name, value } = e.target;
+    setLoginValue({
+      ...loginValue,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = () => {
+    console.log(id, password);
+  };
+
   return (
     <div>
-      <input type="text" />
-      <input type="password" />
-      <button>로그인</button>
+      <input
+        name="id"
+        type="text"
+        placeholder="아이디를 입력해주세요."
+        value={id}
+        onChange={handleLoginInput}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="비밀번호를 입력해주세요."
+        value={password}
+        onChange={handleLoginInput}
+      />
+      <button onClick={handleLogin}>로그인</button>
     </div>
   );
 }
