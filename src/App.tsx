f01b07781ee152ed5f8a3fe287030d8505e15f39
jy0813 <busbug3@gmail.com>
2023-10-06 20:42:20 +0900

    style: style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,8 +31,10 @@ const router = createBrowserRouter([
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
